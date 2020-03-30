987403bb8a7ec2d36935b29feb33960aaa9466b5
Ouali Amar <mr.ouali@gmail.com>
2020-03-30 18:22:49 +0100

    Rename solution,project and pkgdef to Bootstrap4vs Restructure snippets files from one folder to multiples folders (one folder by component)

--- Bootstrap4vs/Properties/AssemblyInfo.cs
+++ Bootstrap4vs/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bootstrap4Snippets")]
+[assembly: AssemblyTitle("Bootstrap4vs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Bootstrap4Snippets")]
+[assembly: AssemblyProduct("Bootstrap4vs")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
